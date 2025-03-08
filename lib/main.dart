af830fea61a62db99306fc89ef6ae02a6c75824d
selingl <selin.gilgil@gmail.com>
2025-03-09 02:42:53 +0300

    change the color to tealAccent in build function of MyAppInClass

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   f();
-  runApp(new MyAppInClass());
+  runApp(new MyAppInClass()); //runApp gets a Widget class which is
+                              // user interface element and make it an app
 
 }
 
 void f() {
   print('another function');
   Employee e = Employee();
-  setNameToJack(e);
+  Person p = e;
+  setNameToJack(p);
   e.jobTitle='software engineer';
   print(e.name);
   print(e.jobTitle);
 }
 
 void setNameToJack(Person e) {
-  e.name='Selin';
+  e.name='Jack';
 }
 
 class Person {
@@ -36,22 +38,7 @@ class MyAppInClass extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
