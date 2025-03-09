a091ecb9c79302a728e24a13ba1618ad16130634
selingl <selin.gilgil@gmail.com>
2025-03-09 03:03:54 +0300

    added new String to StatefulWidget of MyHomePage which interacts when the button is clicked

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String myCat = 'Duman';
 
   void _incrementCounter() {
     setState(() {
@@ -74,6 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      myCat = 'Lina';
     });
   }
 
@@ -117,6 +119,10 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              myCat,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
